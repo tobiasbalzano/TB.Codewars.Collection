5facd75857d6cf80669fb3aa44cc6126b32ff747
Tobias Balzano <tobias.balzano@outlook.com>
2016-11-30 18:59:12 +0100

    initial commit

---
+++ 7Kyu.Tests/TargetDateTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using _7Kyu;
+
+[TestFixture]
+public static class DateDaysTests
+{
+    private static void testing(string actual, string expected)
+    {
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public static void test1()
+    {
+        testing(DateDays.DateNbDays(4281, 5087, 2), "2024-07-03");
+        testing(DateDays.DateNbDays(4620, 5188, 2), "2021-09-19");
+        testing(DateDays.DateNbDays(9999, 11427, 6), "2018-03-13");
+        testing(DateDays.DateNbDays(3525, 4822, 3), "2026-04-18");
+        testing(DateDays.DateNbDays(5923, 6465, 6), "2017-06-10");
+        testing(DateDays.DateNbDays(4254, 4761, 8), "2017-05-22");
+        testing(DateDays.DateNbDays(1244, 2566, 4), "2033-11-04");
+        testing(DateDays.DateNbDays(6328, 7517, 5), "2019-05-25");
+        testing(DateDays.DateNbDays(2920, 3834, 2), "2029-06-03");
+        testing(DateDays.DateNbDays(7792, 8987, 4), "2019-07-09");
+    }
+}
---
+++ TB.Codewars.Executable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _7Kyu;
+namespace TB.Codewars.Executable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateDays.DateNbDays(110F, 1110F, 1F);
+        }
+    }
+}
